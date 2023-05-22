5c822885fccaa66de6cf25b61d6c8b003c11e0af
Idan Rosenberg <idanrs@mta.ac.il>
2023-05-22 19:28:28 +0300

    add vehicle validator class

--- Garage/DTO.cs
+++ Garage/DTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Ex03.GarageLogic.Motorcycle;
+﻿using static Ex03.GarageLogic.Motorcycle;
 
 namespace Ex03.GarageLogic
 {
--- Garage/GarageLogicEngine/SupportedVehicleValidator.cs
+++ Garage/GarageLogicEngine/SupportedVehicleValidator.cs
@@ -4,14 +4,48 @@ namespace Ex03.GarageLogic
 {
     class SupportedVehicleValidator
     {
-        List<Vehicle> m_SupportedVehicleInGarageList;
+        const string k_placeholderForString = "";
+        const int k_placeholderInteger = -1;
+        const bool k_placeholderIsCaryingDangeriousMaterials = true;
+        const Motorcycle.eLicenseType k_placeholderLisenceType = Motorcycle.eLicenseType.A1;
 
+        VehicleComponentFactory m_vehicleComponentFactory = new VehicleComponentFactory();
+        List<Vehicle> m_SupportedVehicleInGarageList;
+        
         public SupportedVehicleValidator()
         {
+            Vehicle vehicle;
 
+            vehicle = new Motorcycle(k_placeholderLisenceType, k_placeholderInteger, k_placeholderForString, k_placeholderForString, 2);
+            vehicle.Engine = m_vehicleComponentFactory.createEngine(Engine.eEnergyType.Octan98, 6.4f);
+            vehicle.Wheels = m_vehicleComponentFactory.createWheelsCollection(2, k_placeholderForString, 31);
+            m_SupportedVehicleInGarageList.Add(vehicle);
+
+            vehicle = new Motorcycle(k_placeholderLisenceType, k_placeholderInteger, k_placeholderForString, k_placeholderForString, 2);
+            vehicle.Engine = m_vehicleComponentFactory.createEngine(Engine.eEnergyType.Electricity, 2.6f);
+            vehicle.Wheels = m_vehicleComponentFactory.createWheelsCollection(2, k_placeholderForString, 31);
+            m_SupportedVehicleInGarageList.Add(vehicle);
+
+            vehicle = new Car(k_placeholderForString, 2, k_placeholderForString, k_placeholderForString, 5);
+            vehicle.Engine = m_vehicleComponentFactory.createEngine(Engine.eEnergyType.Octan95, 46);
+            vehicle.Wheels = m_vehicleComponentFactory.createWheelsCollection(5, k_placeholderForString, 33);
+            m_SupportedVehicleInGarageList.Add(vehicle);
+
+            vehicle = new Car(k_placeholderForString, 2, k_placeholderForString, k_placeholderForString, 5);
+            vehicle.Engine = m_vehicleComponentFactory.createEngine(Engine.eEnergyType.Electricity, 5.2f);
+            vehicle.Wheels = m_vehicleComponentFactory.createWheelsCollection(5, k_placeholderForString, 33);
+            m_SupportedVehicleInGarageList.Add(vehicle);
+
+            vehicle = new Truck(k_placeholderIsCaryingDangeriousMaterials, k_placeholderInteger, k_placeholderForString, k_placeholderForString, 14);
+            vehicle.Engine = m_vehicleComponentFactory.createEngine(Engine.eEnergyType.Soler, 135);
+            vehicle.Wheels = m_vehicleComponentFactory.createWheelsCollection(14, k_placeholderForString, 26);
+            m_SupportedVehicleInGarageList.Add(vehicle);
         }
 
+        private bool compare()
+        {
 
-
+            return false;
+        }
     }
 }
